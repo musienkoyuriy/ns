3651f6e350d111192d03dbbeb4938e784c654c66
Yuriy Musienko <musienkoyuriy@gmail.com>
2022-02-21 21:27:13 +0200

    parse env variables

--- src/lib/services/execute.ts
+++ src/lib/services/execute.ts
@@ -1,13 +1,34 @@
 import { spawn } from 'child_process';
+import { findLastIndex, trimQuotes } from '../utils';
+
+const ENV_VARIABLE_REGEXP = /\w\s{0,}=\s{0,}\'{0,}\"{0,}\w\'{0,}\"{0,}/;
 
 const mapCommandToSpawnReadable = (script: string) => {
   const splittedCommand = script.split(' ');
-  const command = splittedCommand[0];
-  const args = splittedCommand.slice(1);
+  const envVarsLastIndex = findLastIndex(splittedCommand, token => ENV_VARIABLE_REGEXP.test(token));
+
+  const envVars = envVarsLastIndex !== -1 ?
+    splittedCommand.slice(0, envVarsLastIndex + 1)
+      .reduce((acc: { [key: string]: string }, envVar: string) => {
+        const [variable, value] = envVar.split('=');
+
+        acc = {
+          ...acc,
+          [variable]: trimQuotes(value)
+        };
+
+        return acc;
+      }, {}) : {};
+
+  const command = envVarsLastIndex !== -1 ? splittedCommand[envVarsLastIndex + 1] : splittedCommand[0];
+  const args = splittedCommand.slice(
+    envVarsLastIndex !== -1 ? envVarsLastIndex + 1 : 1
+  );
 
   return {
     command,
-    args
+    args,
+    envVars
   };
 }
 
@@ -16,16 +37,21 @@ export const execute = (script: string): void => {
     return;
   }
 
-  const { command, args } = mapCommandToSpawnReadable(script);
+  const { command, args, envVars } = mapCommandToSpawnReadable(script);
 
-  const child = spawn(command, args);
+  const child = spawn(command, args, {
+    env: {
+      ...process.env,
+      ...envVars
+    }
+  });
 
   child.stdout.on('data', data => {
     process.stdout.write(data);
   });
 
   child.stderr.on('data', err => {
-    console.log(`stderr: `, err);
+    process.stdout.write(`stderr: `, err);
   });
 
   child.on('exit', (code, signal) => {
